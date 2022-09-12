2b8d5ae8d06ee021dc452be7cbdfcf9800ea7d01
Brian Jun <bmtef.02@gmail.com>
2022-09-12 16:15:53 -0700

    deleted heroku url and added netlify url to whitelist

--- routes/cors.js
+++ routes/cors.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 
-const whitelist = ["http://localhost:3000", "http://localhost:3001", "https://dry-cliffs-03397.herokuapp.com/"];
+const whitelist = ["http://localhost:3000", "http://localhost:3001", "https://casting-card-client.netlify.app"];
 const corsOptionsDelegate = (req, callback) => {
     let corsOptions;
     console.log(req.header("Origin"));
